f49d0865b37d8bf7274557acaaf9b1f5bc573916
kimgayoung <gayoungkim2508@gmail.com>
2025-12-26 15:42:15 +0900

    refactor: Remove deprecated SQL migration scripts and optimize schema

--- supabase-cleanup-todos-category.sql
+++
@@ -1,4 +0,0 @@
--- Remove deprecated 'category' text column from todos table
--- (Replaced by todo_category_id foreign key)
-
-ALTER TABLE todos DROP COLUMN IF EXISTS category;
--- supabase-migration-todo-category-color.sql
+++
@@ -1,2 +0,0 @@
--- Add color column to todo_categories table
-ALTER TABLE todo_categories ADD COLUMN IF NOT EXISTS color VARCHAR(7) DEFAULT '#4a9eff';
